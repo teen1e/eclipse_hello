89025b93e1028d98fdc9815333084a822a55bce8
MinJi <mj_x_x@naver.com>
2016-11-14 10:48:26 +0900

    modified PushPrint.java on local repository

--- helloWorld/src/helloWorld/PushPrint.java
+++ helloWorld/src/helloWorld/PushPrint.java
@@ -4,12 +4,10 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("Add Java Class File Push");
-=======
-		system.out.println("Add Java Class File Push");
-		system.out.println("World with 365");
->>>>>>> branch 'master' of https://github.com/teen1e/eclipse_hello.git
+		System.out.println("World with 365");
+		System.out.println("world with local");
+		
 	}
 
 }
