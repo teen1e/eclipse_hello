7c765b0360f0e2112d901c314193052e18a6e47d
teen1e <mj_x_x@naver.com>
2016-11-14 10:32:09 +0900

    PushPrint.java modified on remote repository

--- helloWorld/src/helloWorld/PushPrint.java
+++ helloWorld/src/helloWorld/PushPrint.java
@@ -4,7 +4,8 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		system.out.println("Add Java Class File Push");
+		system.out.println("World with 365");
 	}
 
 }
